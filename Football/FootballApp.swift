d82792e048803f1d673dc1520a0b0434e348a556
Ajay Yamamoto <atyamamoto226@gmail.com>
2025-11-16 13:04:33 -0700

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Ajay Yamamoto on 2025-11-16.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
